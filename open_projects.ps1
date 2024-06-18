104c73bd7943a2416f1758259cc03e38b190dcb1
Ashanka Randeniya <ashanka@ascentic.se>
2024-06-18 14:28:00 +0530

    Update open solution ps file

--- open_projects.ps1
+++ open_projects.ps1
@@ -1,6 +1,6 @@
 code .\Play.Infra
-code .\Play.Catalog\src\Play.Catalog.Service
-code .\Play.Inventory\src\Play.Inventory.Service
+code .\Play.Catalog\src
+code .\Play.Inventory\src
 code .\Play.Common\src\Play.Common
 
 code .\Play.Identity\src\Play.Identity.Service
