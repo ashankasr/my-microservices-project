1c5089c4e488400d5612a138caf51fedc22955f2
Ashanka Randeniya <ashanka@ascentic.se>
2024-06-18 12:01:24 +0530

    powershell script to open the solutions

---
+++ open_projects.ps1
@@ -0,0 +1,7 @@
+code .\Play.Infra
+code .\Play.Catalog\src\Play.Catalog.Service
+code .\Play.Inventory\src\Play.Inventory.Service
+code .\Play.Common\src\Play.Common
+
+code .\Play.Identity\src\Play.Identity.Service
+code .\Play.Frontend
